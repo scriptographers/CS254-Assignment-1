ddcf56a42f7a7a29dfcb9b62cdb8e5f3cb2952ee
Harshit Varma <61575521+hrshtv@users.noreply.github.com>
2021-02-03 10:23:21 +0530

    Added comments

--- BasicLogicGate.vhd
+++ BasicLogicGate.vhd
@@ -1,24 +1,25 @@
--- An entity is a basic building block, a "black box" with inputs and outputs specified 
-entity BasicLogicGate is
-	port(
+-- An entity is a basic building block, a "black box" with inputs and outputs specified. Analogous to C/C++ classes 
+entity BasicLogicGate is -- Used for defining inputs and outputs
+	port( -- Specifies the i/o ports to the logic gate 
 		-- Inputs
-		X: in bit;
+		X: in bit; -- bit is an in-built data type
 		Y: in bit;
 		
 		-- Outputs: Instead of having 3 separate projects for each gate, we define all in a single project
 		notX : out bit; 
 		XorY : out bit;
-		XandY: out bit
+		XandY: out bit 
 	);
 end entity BasicLogicGate;
 
 
--- Defines the structure of the above entity
-architecture Struct of BasicLogicGate is
-begin
+-- Defines the structure/implementation/functionality of the above entity
+architecture Struct of BasicLogicGate is -- "Struct" is the name of the architecture
+begin 
+	-- All these happen concurrently, these are signals and not variables
 
 	-- NOT Gate
-	notX <= (not X);
+	notX <= (not X); -- "<=" is the signal assignment operator
 	
 	-- OR Gate
 	XorY <= (X or Y);
